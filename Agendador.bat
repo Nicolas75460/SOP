f994f0161dfcefa7e7c620630ed25489cb56c4f2
Nícolas Oliveira Carvalho da Silva <159180162+Nicolas75460@users.noreply.github.com>
2024-03-14 12:53:26 -0300

    Add files via upload

---
+++ Agendador.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+@echo.
+@echo ********************************
+@echo ***                          ***
+@echo ***    HORA DO ALMOCO  :D    ***
+@echo ***                          ***
+@echo ********************************
+@echo.
+pause
